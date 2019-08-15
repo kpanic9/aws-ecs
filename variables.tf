33528fac733858b26f8fd27985852654234cc9e1
Namesh Sanjitha <nameshsanjitha.maguragamaathige@news.com.au>
2019-08-15 12:32:20 +1000

    Added vpc design paramters

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "vpc_cidr" {
+	description = "ECS VPC CIDR"
+}
+
+variable "public_subnet_a_ip_range" {
+	description = "Public subnet a ip address range"
+}
+
+variable "public_subnet_b_ip_range" {
+	description = "Public subnet b ip address range"
+}
+
+variable "public_subnet_c_ip_range" {
+	description = "Public subnet c ip address range"
+}
+
+variable "private_subnet_a_ip_range" {
+	description = "Private subnet a ip address range"
+}
+
+variable "private_subnet_b_ip_range" {
+	description = "Private subnet b ip address range"
+}
+
+variable "private_subnet_c_ip_range" {
+	description = "private subnet c ip address range"
+}
+
