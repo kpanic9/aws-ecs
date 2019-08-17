f5bab20fcb72c2c7503895d7998fbd2523e2bfb5
Namesh Sanjitha <tns.xiii@gmail.com>
2019-08-17 10:16:50 +0530

    Addded private subnet routing

--- network.tf
+++ network.tf
@@ -80,4 +80,74 @@ resource "aws_route_table_association" "public_subnet_c_association" {
 }
 
 
+# vpc private/secure subnet and routing configuration
+resource "aws_subnet" "private_a" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.private_subnet_a_ip_range}"
+    availability_zone = "ap-southeast-2a"
 
+    tags = {
+        Name = "PrivateSubnetA"
+    }
+}
+
+resource "aws_subnet" "private_b" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.private_subnet_b_ip_range}"
+    availability_zone = "ap-southeast-2b"
+
+    tags = {
+        Name = "PrivateSubnetB"
+    }
+}
+
+resource "aws_subnet" "private_c" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.private_subnet_c_ip_range}"
+    availability_zone = "ap-southeast-2c"
+
+    tags = {
+        Name = "PrivateSubnetC"
+    }
+}
+
+resource "aws_eip" "nat_gw_eip" {
+    vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = "${aws_eip.nat_gw_eip.id}"
+    subnet_id = "${aws_subnet.public_a.id}"
+
+    tags = {
+        Name = "EcsVpcNatGW"
+    }
+}
+
+resource "aws_route_table" "private_subnet_rt" {
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_nat_gateway.nat_gw.id}"
+    }
+
+    tags = {
+        Name = "PrivateSubnetRT"
+    }
+}
+
+resource "aws_route_table_association" "private_subnet_a_association" {
+    subnet_id = "${aws_subnet.private_a.id}"
+    route_table_id = "${aws_route_table.private_subnet_rt.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_b_association" {
+    subnet_id = "${aws_subnet.private_b.id}"
+    route_table_id = "${aws_route_table.private_subnet_rt.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_c_association" {
+    subnet_id = "${aws_subnet.private_c.id}"
+    route_table_id = "${aws_route_table.private_subnet_rt.id}"
+}
