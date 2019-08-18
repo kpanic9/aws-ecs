4309a8e8eaae5543e60f68e16c26d1cd1d178282
Namesh Sanjitha <tns.xiii@gmail.com>
2019-08-18 06:05:36 +0530

    Added external alb sg

--- sg.tf
+++ sg.tf
@@ -29,3 +29,34 @@ resource "aws_security_group" "bastion_host_sg" {
         Name = "BastionHostSG"
     }
 }
+
+# external alb security group, http and https traffic is allowed from anywhere
+resource "aws_security_group" "external_alb_sg" {
+    name = "ExternalAlbSG"
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ExternalAlbSG"
+    }
+}
