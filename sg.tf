970b4eebda959148c23354d7424d81c951be06e4
Namesh Sanjitha <tns.xiii@gmail.com>
2019-08-18 06:07:02 +0530

    Added rds db host security group

--- sg.tf
+++ sg.tf
@@ -99,3 +99,27 @@ resource "aws_security_group" "app_sg" {
         Name = "ApplicationSG"
     }
 }
+
+# database security group, ingress is allowed from ecs nodes
+resource "aws_security_group" "database_sg" {
+    name = "DatabaseSG"
+    vpc_id = "${aws_vpc.vpc.id}"
+    
+    ingress {
+        from_port = 5432
+        to_port  = 5432
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.app_sg.id}"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "DatabaseSG"
+    }
+}
