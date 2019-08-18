3804dea5a80aa306418277a2bf3f135a8e0f2fd6
Namesh Sanjitha <tns.xiii@gmail.com>
2019-08-18 06:06:28 +0530

    Added ECS cluster node security group

--- sg.tf
+++ sg.tf
@@ -60,3 +60,42 @@ resource "aws_security_group" "external_alb_sg" {
         Name = "ExternalAlbSG"
     }
 }
+
+# ecs node security group
+resource "aws_security_group" "app_sg" {
+    name = "${var.environment}ApplicationSG"
+    vpc_id = "${data.aws_vpc.finda_vpc.id}"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        self = true
+        security_groups = ["${aws_security_group.external_alb_sg.id}"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        self = true
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.bastion_host_sg.id}"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ApplicationSG"
+    }
+}
