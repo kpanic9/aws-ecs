722d18453f2c35cb2a2793ade3307202b7608ee5
Namesh Sanjitha <tns.xiii@gmail.com>
2019-09-22 07:12:54 +0530

    Added loadbalacer to expose the service

--- ecs.tf
+++ ecs.tf
@@ -174,3 +174,27 @@ resource "aws_lb_target_group" "ecs_app_target_group" {
         Name = "EcsAppTargetFroup"
     }
 }
+
+# application load balancer for ecs container instances
+resource "aws_lb" "ecs_application_lb" {
+    name = "${var.environment}EcsApplicationLB"
+    internal = true
+    load_balancer_type = "application"
+    security_groups = ["${aws_security_group.app_sg.id}"]
+    subnets = ["${aws_subnet.private_a.id}", "${aws_subnet.private_b.id}", "${aws_subnet.private_c.id}"]
+    idle_timeout = 720
+    tags = {
+        Name = "EcsApplicationLayerLB"
+    }
+}
+
+resource "aws_lb_listener" "ecs_application_lb_listner" {
+    load_balancer_arn = "${aws_lb.ecs_application_lb.arn}"
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "forward"
+        target_group_arn = "${aws_lb_target_group.ecs_app_target_group.arn}"
+    }
+}
