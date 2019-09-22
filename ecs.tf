3b96914004635e5d1ed2676dc4fcb67eaca063ca
Namesh Sanjitha <tns.xiii@gmail.com>
2019-09-22 07:10:15 +0530

    added ecs service with the target group

--- ecs.tf
+++ ecs.tf
@@ -138,3 +138,39 @@ resource "aws_ecs_task_definition" "task_definition" {
         Name = "TaskDefinition"
     }
 }
+
+resource "aws_ecs_service" "service" {
+    name = "nginx-app"
+    cluster = "${aws_ecs_cluster.app_cluster.id}"
+    task_definition = "${aws_ecs_task_definition.task_definition.arn}"
+    scheduling_strategy = "DAEMON"
+
+    load_balancer {
+        target_group_arn = "${aws_lb_target_group.ecs_app_target_group.arn}"
+        container_name = "web"
+        container_port = "80"
+    }
+
+}
+
+resource "aws_lb_target_group" "ecs_app_target_group" {
+    name = "EcsAppTargetFroup"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = "${aws_vpc.vpc.id}"
+    
+    health_check {
+        protocol = "HTTP"
+        path = "/"
+        port = "traffic-port"
+        healthy_threshold = 5
+        unhealthy_threshold = 2
+        timeout = 5
+        interval = 30
+        matcher = "200"
+    }
+
+    tags = {
+        Name = "EcsAppTargetFroup"
+    }
+}
